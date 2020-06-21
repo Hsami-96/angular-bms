cff8ef6556c421e55d1ef9e0a6e7da980c4eabb4
Hani Samir <Hani.SAMIR@freshfields.com>
2020-06-21 15:56:06 +0100

    added firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { BmsLoginComponent } from './bms-login/bms-login.component';
 import { BmsLoginModule } from './bms-login/bms-login.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { BmsLoginModule } from './bms-login/bms-login.module';
   ],
   imports: [
     BrowserModule,
-    BmsLoginModule
+    BmsLoginModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
